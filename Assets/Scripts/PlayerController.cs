6ff3e2a86a64603caaf566d22b11c3f3761d67f3
Laantje <laan.j.david@gmail.com>
2016-02-09 11:11:49 +0100

    Added *WIP* Unity Game 'Power Laser' to git

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject gun;
+    public Animator shootanim;
+    public bool shooting;
+    public bool ADS;
+
+	// Use this for initialization
+	void Start () {
+        shooting = false;
+        ADS = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Shooting mechanics
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!ADS)
+            {
+                Debug.Log("Pressed left click.");
+                gun.GetComponent<Animator>().Play("Gun_Shoot");
+                //shooting = true;
+            }
+            else if (ADS)
+            {
+                Debug.Log("Pressed left click.");
+                gun.GetComponent<Animator>().Play("GunADS_Shoot");
+                //shooting = true;
+            }
+        }
+
+        //Aim mechanics
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!ADS && !shooting)
+            {
+                Debug.Log("Pressed right click.");
+                gun.GetComponent<Animator>().Play("GunToADS");
+                ADS = true;
+            } 
+            else if (ADS && !shooting)
+            {
+                Debug.Log("Pressed right click.");
+                gun.GetComponent<Animator>().Play("GunFromADS");
+                ADS = false;
+            }
+        }
+
+    }
+}
