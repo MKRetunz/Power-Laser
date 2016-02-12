32709d9b6a94c96e1ed32d341c681eb28664e31c
Laantje <laan.j.david@gmail.com>
2016-02-12 13:24:48 +0100

    Added Martijn's code.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,16 @@ public class PlayerController : MonoBehaviour
     public static bool ADS;
     public static bool switchADS;
     private float shootDelay;
+    public static bool OverHeat;
+    private bool CanCover;
+    private bool Covering;
+    private bool showText;
+    public Slider HeatSlider;
+    public Text CoverPopUp;
+    public float crouchingSpeed;
+    public float CspeedUp;
+    public float GunHeat;
+    public float TimerCover;
 
     // Use this for initialization
     void Start()
@@ -16,6 +27,26 @@ public class PlayerController : MonoBehaviour
         shooting = false;
         switchADS = false;
         ADS = false;
+        crouchingSpeed = 0.1f;
+        CspeedUp = crouchingSpeed;
+        GunHeat = 0.0f;
+        showText = false;
+        TimerCover = 0.0f;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Collider>().name == "Cover")
+        {
+            Debug.Log("I'm Working");
+            showText = true;
+            TimerCover = 0.0f;
+            CanCover = true;
+        }
+        if (col.GetComponent<Collider>().name == "Uncover")
+        {
+            CanCover = false;
+        }
     }
 
     // Update is called once per frame
@@ -28,18 +59,18 @@ public class PlayerController : MonoBehaviour
         }*/
 
         // Shoot delay
-        if(shooting && shootDelay < 0.8f)
+        if (shooting && shootDelay < 0.8f)
         {
             shootDelay += 1 * Time.deltaTime;
         }
-        else if(shooting && shootDelay >= 0.8f)
+        else if (shooting && shootDelay >= 0.8f)
         {
             shootDelay = 0;
             shooting = false;
         }
 
         //Shooting mechanics
-        if (Input.GetMouseButtonDown(0) && !shooting && !switchADS)
+        if (Input.GetMouseButtonDown(0) && !shooting && !switchADS && !OverHeat)
         {
             if (!ADS)
             {
@@ -51,7 +82,48 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Pressed left click.");
                 gun.GetComponent<Animator>().Play("GunADS_Shoot");
             }
+            GunHeat += Time.deltaTime * 20;
         }
+
+        //Crouching
+        if (Input.GetKey("x"))
+        {
+            CspeedUp -= crouchingSpeed * (Time.deltaTime * 10);
+            if (CspeedUp < -0.6f)
+            {
+                CspeedUp = -0.6f;
+            }
+        }
+
+        //Cover
+        else if (CanCover == true && Input.GetKey("z"))
+        {
+            CspeedUp = -0.6f;
+        }
+        else
+        {
+            CspeedUp += crouchingSpeed * (Time.deltaTime * 10);
+            if (CspeedUp > 0.0f)
+            {
+                CspeedUp = 0.0f;
+            }
+        }
+        transform.localPosition = new Vector3(0, CspeedUp, 0);
+        GunHeat -= Time.deltaTime / 2;
+        TimerCover += Time.deltaTime;
+
+        if (TimerCover > 3.0)
+        {
+            showText = false;
+        }
+
+        if (GunHeat < 0.0f)
+        {
+            GunHeat = 0.0f;
+            OverHeat = false;
+        }
+        if (GunHeat > 1.5f) { OverHeat = true; }
+        HeatSlider.value = GunHeat;
 
         //Aim mechanics
         if (Input.GetMouseButtonDown(1))
@@ -72,5 +144,16 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+    }
+
+    void OnGUI()
+    {
+        if (showText)
+        {
+            GUIStyle Coverstyle = new GUIStyle();
+            Coverstyle.alignment = TextAnchor.MiddleCenter;
+            Coverstyle.fontSize = 50;
+            GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height - 40, 400, 30), "Hold Z for cover.", Coverstyle);
+        }
     }
 }
