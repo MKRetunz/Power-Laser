2d7565ac0f207873f636a4d82a026bc7ca197f0f
MKRetunz <Martijnklok@hotmail.nl>
2016-02-18 15:15:00 +0100

    Added an array for gun selection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    //Guns
-    public GameObject gun;
-    public GameObject rifle;
+    //pistol
+    public GameObject singleShotP;
+    public GameObject burstShotP;
+    public GameObject revolverP;
+
+    //rifles
+    public GameObject semiAutoR;
+    public GameObject fullAutoR;
+    public GameObject boltActionR;
+
+    //Shotguns
+    public GameObject pumpActionS;
+    public GameObject semiAutoS;
+
+    //array
+    private GameObject[] gunArray;
     
     //Bools
     public static bool shooting;
@@ -26,6 +39,7 @@ public class PlayerController : MonoBehaviour
     public float hSliderValue = 0;
     private float shootDelay;
     public float currentGun;
+    public int maxGuns;
 
     //UI
     public Slider HeatSlider;
@@ -40,13 +54,30 @@ public class PlayerController : MonoBehaviour
         CspeedUp = crouchingSpeed;
         GunHeat = 0.0f;
         currentGun = 0f;
+
+        maxGuns = 8;
      
         showText = false;
         shooting = false;
         switchADS = false;
         ADS = false;
 
-        rifle.SetActive(false);
+        gunArray = new GameObject[maxGuns];
+
+        gunArray[0] = singleShotP;
+        gunArray[1] = burstShotP;
+        gunArray[2] = revolverP;
+        gunArray[3] = semiAutoR;
+        gunArray[4] = fullAutoR;
+        gunArray[5] = boltActionR;
+        gunArray[6] = pumpActionS;
+        gunArray[7] = semiAutoS;
+
+        for (int i = 1; i < maxGuns; i++)
+        {
+            gunArray[i].SetActive(false);
+
+        }
     }
 
     void OnTriggerEnter(Collider col)
@@ -90,12 +121,12 @@ public class PlayerController : MonoBehaviour
             if (!ADS)
             {
                 Debug.Log("Pressed left click.");
-                gun.GetComponent<Animator>().Play("Gun_Shoot");
+                singleShotP.GetComponent<Animator>().Play("Gun_Shoot");
             }
             else if (ADS)
             {
                 Debug.Log("Pressed left click.");
-                gun.GetComponent<Animator>().Play("GunADS_Shoot");
+                singleShotP.GetComponent<Animator>().Play("GunADS_Shoot");
             }
             GunHeat += Time.deltaTime * 20;
         }
@@ -108,6 +139,36 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             switchGun(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            switchGun(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            switchGun(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            switchGun(4);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            switchGun(5);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            switchGun(6);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            switchGun(7);
         }
 
         //Crouching
@@ -156,14 +217,14 @@ public class PlayerController : MonoBehaviour
             if (!ADS && !shooting && !switchADS)
             {
                 Debug.Log("Pressed right click.");
-                gun.GetComponent<Animator>().Play("GunToADS");
+                singleShotP.GetComponent<Animator>().Play("GunToADS");
                 switchADS = true;
                 ADS = true;
             }
             else if (ADS && !shooting && !switchADS)
             {
                 Debug.Log("Pressed right click.");
-                gun.GetComponent<Animator>().Play("GunFromADS");
+                singleShotP.GetComponent<Animator>().Play("GunFromADS");
                 switchADS = true;
                 ADS = false;
             }
@@ -173,19 +234,32 @@ public class PlayerController : MonoBehaviour
     //Weapon changing
     void switchGun(int newWeapon)
     {
-        //Add an Array
-        if(newWeapon == 0)
+        for (int i = 0; i < maxGuns; i++)
         {
-            gun.SetActive(false);
-            rifle.SetActive(true);
-            currentGun = 1f;
+            gunArray[i].SetActive(false);
+
+            if(i == newWeapon)
+            {
+                gunArray[i].SetActive(true);
+            }
+        }
+
+        /*Add an Array
+        if (newWeapon == 0)
+        {
+            gun.SetActive(true);
+            semiAutoR.SetActive(false);
+            currentGun = 0f;
         }
         if(newWeapon == 1)
         {
-            gun.SetActive(true);
-            rifle.SetActive(false);
-            currentGun = 0f;
-        }
+            gun.SetActive(false);
+            semiAutoR.SetActive(true);
+            currentGun = 1f;
+        }*/
+
+
+        
     }
 
     void OnGUI()
