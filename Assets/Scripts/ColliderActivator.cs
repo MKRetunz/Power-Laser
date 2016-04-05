037a7d3a1227f7d61e3ce936ce8d4bfdbed783c5
MKRetunz <Martijnklok@hotmail.nl>
2016-04-05 11:33:35 +0200

    Added Lasergate

--- Assets/Scripts/ColliderActivator.cs
+++ Assets/Scripts/ColliderActivator.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ColliderActivator : MonoBehaviour {
 
+    float timer = 0.0f;
+    bool down = false;
 	// Use this for initialization
 	void Start () {
         
@@ -10,6 +12,23 @@ public class ColliderActivator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        timer += Time.deltaTime;
+
+	    if (timer >= 1.5f && down == false)
+        {
+            transform.localPosition = new Vector3(7.5f, -5, 0);
+
+            down = true;
+
+        }
+        if (timer >= 3.0f && down == true)
+        {
+            transform.localPosition = new Vector3(7.5f, 0, 0);
+
+            down = false;
+
+            timer = 0.0f;
+        }
 	}
 }
