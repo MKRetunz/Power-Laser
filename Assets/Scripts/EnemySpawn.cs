ba42eef328389981007f436e0e50188f83d80f1c
MKRetunz <Martijnklok@hotmail.nl>
2016-04-05 12:31:39 +0200

    Added enemy spawn

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class EnemySpawn : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    float timer;
+    public float targetTime = 3.0f;
+    public Rigidbody blueprint;
+
+    // Use this for initialization
+    void Start () {
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        timer += Time.deltaTime;
+
+        if (timer >= targetTime)
+        {
+            Rigidbody enemy = Instantiate(blueprint, transform.position, transform.rotation) as Rigidbody;
+            timer = 0.0f;
+        }
+
+    }
 }
