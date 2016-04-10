be4a29e8e00bfaaa4e3e8a2f0fc7aec84445525d
MKRetunz <Martijnklok@hotmail.nl>
2016-04-10 17:23:24 +0200

    Changed spawning & lasergate

--- Assets/Scripts/ColliderActivator.cs
+++ Assets/Scripts/ColliderActivator.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class ColliderActivator : MonoBehaviour {
 
+
+    Vector3 location; 
     float timer = 0.0f;
     bool down = false;
 	// Use this for initialization
 	void Start () {
-        
+        location = this.transform.position;
 	}
 	
 	// Update is called once per frame
@@ -15,16 +17,21 @@ public class ColliderActivator : MonoBehaviour {
 
         timer += Time.deltaTime;
 
-	    if (timer >= 1.5f && down == false)
+        Vector3 newLocation = location;
+
+        newLocation.y -= 105;
+
+
+        if (timer >= 1.5f && down == false)
         {
-            transform.localPosition = new Vector3(7.5f, -5, 0);
+            transform.position = new Vector3(location.x, newLocation.y, location.z);
 
             down = true;
 
         }
         if (timer >= 3.0f && down == true)
         {
-            transform.localPosition = new Vector3(7.5f, 0, 0);
+            transform.position = location;
 
             down = false;
 
--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,25 +4,58 @@ using System.Collections;
 public class EnemySpawn : MonoBehaviour {
 
     float timer;
-    public float targetTime = 3.0f;
+    public int wave;
+    public int maxEnemies;
+    bool waveactivator;
+    int i;
+    public float targetTime = 0.1f;
     public Rigidbody blueprint;
 
     // Use this for initialization
     void Start () {
-
-
+        wave = 1;
+        maxEnemies = 10;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Collider>().name == "Target(Clone)")
+        {
+            
+        }
+
+        else
+        {
+            newWave();
+        }
+    }
+
+        // Update is called once per frame
+        void Update () {
 
         timer += Time.deltaTime;
 
-        if (timer >= targetTime)
+        if (timer >= targetTime && i < maxEnemies)
         {
             Rigidbody enemy = Instantiate(blueprint, transform.position, transform.rotation) as Rigidbody;
             timer = 0.0f;
+            i++;
+
+            waveactivator = true;
         }
 
+        
+
+    }
+
+    void newWave ()
+    {
+        if (waveactivator)
+        {
+            wave++;
+            i = 0;
+            maxEnemies = wave * 10;
+            waveactivator = false;
+        }
     }
 }
